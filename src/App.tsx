64efd101781394f8acbf4be502ccc406a70c2a97
LukaLuxx <luka.jambrosic18@gmail.com>
2023-04-21 17:10:07 +0200

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="todo-text">
+                <h1>Taskmaster</h1>
+                <div className="todo-input">
+                    <button className="blue">Task &rarr; </button>
+                    <input type="text" placeholder="Write your task here..." />
+                    <button className="red">Created:</button>
+                    <input
+                        type="number"
+                        name="created"
+                        id="created"
+                        placeholder="Write your date here..."
+                    />
+                    <button className="green">Due date:</button>
+                    <input
+                        type="number"
+                        name="due"
+                        id="due"
+                        placeholder="Write your due date here..."
+                    />
+                </div>
+            </div>
+            <div className="task-wrapper">
+                <div className="task-list">
+                    <h2>TODOs &darr;</h2>
+                    <ul>
+                        <li className="blue">
+                            <b>Task:</b>
+                        </li>
+                        <li className="red">
+                            <b>Created:</b>
+                        </li>
+                        <li className="green">
+                            <b>Due:</b>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
